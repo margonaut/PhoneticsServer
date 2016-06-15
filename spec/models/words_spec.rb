10b7f9df5ff851371556820990cba278d7524578
Margaret Morgan <mmogan@datto.com>
2016-06-15 18:53:27 -0400

    Test suite is connecting to db properly now

--- spec/models/words_spec.rb
+++ spec/models/words_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
+require 'rails_helper'
 
-describe Word do
+RSpec.describe Word, type: :model do
+  
   
   it { should validate_presence_of(:text) }
   it { should validate_presence_of(:transcription) }
-  
+
+
 end
