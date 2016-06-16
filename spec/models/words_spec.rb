423db269482a808423700b4525853c662cdd311f
Margaret Morgan <mmogan@datto.com>
2016-06-16 14:17:09 -0400

    Reorganized tests

--- spec/models/words_spec.rb
+++ spec/models/words_spec.rb
@@ -4,21 +4,16 @@ require 'rails_helper'
 RSpec.describe Word, type: :model do
   
   
-  describe "Columns" do
-    it { should validate_presence_of(:text) }
-    it { should validate_presence_of(:transcription) }
-  end
-  
-  describe "Uniqueness" do
+  describe "Text" do
     FactoryGirl.create(:word)
     it { should validate_uniqueness_of(:text) }
-  end
-  
-  describe "Letter values only" do
+    it { should validate_presence_of(:text) }
+    
     it { should_not allow_value("cat1").for(:text) }
     it { should allow_value("catty").for(:text) }
   end
   
-
-
+  describe "Transcription" do
+    it { should validate_presence_of(:transcription) }
+  end
 end
