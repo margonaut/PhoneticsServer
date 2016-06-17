d82c551f50135f1ebb1a07f89456c963d1b52d41
Margaret Morgan <mmogan@datto.com>
2016-06-17 10:45:53 -0400

    Wrote and passed more WordsPhonemes tests

--- app/models/words_phoneme.rb
+++ app/models/words_phoneme.rb
@@ -1,4 +1,7 @@
 class WordsPhoneme < ActiveRecord::Base
   belongs_to :word
   belongs_to :phoneme
+  
+  validates :word, presence: true
+  validates :phoneme, presence: true
 end
--- spec/models/words_phonemes_spec.rb
+++ spec/models/words_phonemes_spec.rb
@@ -4,8 +4,12 @@ require 'spec_helper'
 RSpec.describe WordsPhoneme do
   
   describe "WordsPhoneme" do
-    # it { should validate_presence_of(:) }
     it { should belong_to(:word) }
     it { should belong_to(:phoneme) }
+    
+    it { should validate_presence_of(:word) }
+    it { should validate_presence_of(:phoneme) }
   end
+  
+  
 end
