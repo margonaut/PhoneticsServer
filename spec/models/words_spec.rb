c59c1dc5285849ca804f44af82e803aeb00ff584
Margaret Morgan <mmogan@datto.com>
2016-06-17 11:00:33 -0400

    Reorganized tests and removed requirement for word to contain only A-Z chars

--- spec/models/words_spec.rb
+++ spec/models/words_spec.rb
@@ -6,14 +6,9 @@ RSpec.describe Word, type: :model do
   
   describe "Text" do
     FactoryGirl.create(:word)
-    it { should validate_uniqueness_of(:text) }
     it { should validate_presence_of(:text) }
-    
-    it { should_not allow_value("cat1").for(:text) }
-    it { should allow_value("catty").for(:text) }
-  end
-  
-  describe "Transcription" do
     it { should validate_presence_of(:transcription) }
+    
+    it { should validate_uniqueness_of(:text) }
   end
 end
