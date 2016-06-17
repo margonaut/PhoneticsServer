7ad697a55997f535fe3a7c3b970a9bf30383d296
Margaret Morgan <mmogan@datto.com>
2016-06-17 16:12:47 -0400

    Adjustments

---
+++ db/migrate/20160617150936_create_transcribed_words.rb
@@ -0,0 +1,6 @@
+class CreateTranscribedWords < ActiveRecord::Migration
+  def change
+    create_table :transcribed_words do |t|
+    end
+  end
+end
--- spec/models/phonemes_spec.rb
+++ spec/models/phonemes_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 
 RSpec.describe Phoneme, type: :model do
   
-  describe "Symbol" do
+  describe "Phoneme" do
     it { should validate_presence_of(:symbol) }
     it { should validate_uniqueness_of(:symbol) }
     
     it { should have_many(:words_phonemes) }
   end
-  
 end
