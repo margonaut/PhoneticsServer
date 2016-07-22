f19b7a47e47489699cb2eefdf47855f98f2ff2c0
margonaut <margaret.morgane@gmail.com>
2016-07-22 19:56:09 -0400

    Added test to validates Words belonging to Lists

--- spec/models/lists_spec.rb
+++ spec/models/lists_spec.rb
@@ -4,4 +4,5 @@ require 'rails_helper'
 RSpec.describe List, type: :model do
     it { should validate_presence_of(:name) }
     it { should have_many(:lists_words) }
+    it { should have_many(:words) }
 end
