6371ecb33895786e5ea1643d615f546003193826
margonaut <margaret.morgane@gmail.com>
2016-07-22 19:59:57 -0400

    Added test to validate Phonemes belonging to Words

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,9 @@
 class Word < ActiveRecord::Base
   has_many :words_phonemes
-  
+  has_many :phonemes, through: :words_phonemes
+
   validates :text, presence: true
   validates :transcription, presence: true
-  
+
   validates :text, uniqueness: true
 end
