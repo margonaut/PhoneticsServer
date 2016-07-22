ce19753c1d7f2eab80ea7a72eb3cf73a3a51ad75
margonaut <margaret.morgane@gmail.com>
2016-07-22 18:44:42 -0400

    Ran migration to create lists table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722223313) do
+ActiveRecord::Schema.define(version: 20160722224255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+  end
 
   create_table "phonemes", force: :cascade do |t|
     t.string  "symbol",   null: false
