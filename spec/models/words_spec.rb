749f3bb975d67b05af9a59e4e6edffa3a2335c85
margonaut <margaret.morgane@gmail.com>
2016-07-24 09:03:42 -0400

    Found workaround for shoulda-matchers bug with db-cleaner installed

--- spec/models/words_spec.rb
+++ spec/models/words_spec.rb
@@ -1,18 +1,29 @@
 require 'spec_helper'
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe Word, type: :model do
 
   describe "Word" do
-    FactoryGirl.create(:word)
+
     it { should validate_presence_of(:text) }
     it { should validate_presence_of(:transcription) }
-
-    it { should validate_uniqueness_of(:text) }
 
     it { should have_many(:words_phonemes) }
     it { should have_many(:phonemes) }
 
     it { should have_many(:lists_words) }
+
+    describe "uniqueness" do
+      # Currently separated out due to a bug with
+      # this version of shoulda-matchers
+      before(:each) do
+        Word.destroy_all
+        FactoryGirl.create(:word)
+      end
+      it { should validate_uniqueness_of(:text) }
+
+    end
+
   end
 end
