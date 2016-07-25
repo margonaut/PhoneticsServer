5306d726e466c9daf0171f5c6a3aca29266f2f90
margonaut <margaret.morgane@gmail.com>
2016-07-25 14:01:09 -0400

    Serialized words now include a full phoneme collection

--- app/serializers/word_serializer.rb
+++ app/serializers/word_serializer.rb
@@ -1,3 +1,11 @@
+require "pry"
 class WordSerializer < ActiveModel::Serializer
-  attributes :id, :text, :transcription, :phonemes
+  attributes :id, :text, :transcription, :phoneme_collection
+
+  def phoneme_collection
+    ordered_phonemes = object.words_phonemes
+    ordered_phonemes.map do |character|
+      character.phoneme
+    end
+  end
 end
