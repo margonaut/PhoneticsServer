0440715e69258bf922d3cddd46dc282f0518b8b6
margonaut <margaret.morgane@gmail.com>
2016-07-25 14:04:14 -0400

    Serialized phoneme_collection is now sorted by phoneme position

--- app/serializers/word_serializer.rb
+++ app/serializers/word_serializer.rb
@@ -3,7 +3,7 @@ class WordSerializer < ActiveModel::Serializer
   attributes :id, :text, :transcription, :phoneme_collection
 
   def phoneme_collection
-    ordered_phonemes = object.words_phonemes
+    ordered_phonemes = object.words_phonemes.sort_by(&:position)
     ordered_phonemes.map do |character|
       character.phoneme
     end
