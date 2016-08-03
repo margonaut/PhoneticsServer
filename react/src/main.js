4af2bf736c9a9ff2476ad02b60c9ac5d6ff987ad
margonaut <margaret.morgane@gmail.com>
2016-08-03 05:38:44 -0400

    Basic react components displaying word list grabbed from api endpoint

--- react/src/main.js
+++ react/src/main.js
@@ -3,8 +3,73 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 $(function() {
+
+  var GameBox = React.createClass({
+    loadWordsFromServer: function() {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+    getInitialState: function() {
+      return {data: []};
+    },
+    componentDidMount: function() {
+      this.loadWordsFromServer();
+    },
+    render() {
+      return (
+        <div className="GameBox">
+        {this.state.data.words ?
+           <ListBox data={this.state.data} /> :
+           null
+        }
+        </div>
+      );
+    }
+  });
+
+  var ListBox = React.createClass({
+    render() {
+      var wordNodes = this.props.data.words.map(function(word) {
+        return (
+          <Word text={word.text} transcription={word.transcription} phonemes={word.phonemes}>
+          </Word>
+        );
+      });
+      return (
+        <div className="ListBox">
+        {wordNodes}
+        </div>
+      );
+    }
+  });
+
+  var Word = React.createClass({
+    render() {
+      return (
+        <div className="Word">
+          <div>
+            {this.props.transcription}
+          </div>
+          <div class="">
+            {this.props.text}
+          </div>
+        </div>
+      )
+    }
+  });
+
+
   ReactDOM.render(
-    <h1>Boo yaa</h1>,
+    <GameBox url="/api/lists/1">Boo yddaa</GameBox>,
     document.getElementById('app')
   );
 });
